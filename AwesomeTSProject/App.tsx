39c3fa0baf5fc83a244e38a0ed706c07fa702f75
Alexandre Colas <alexandr.colas@free.fr>
2022-02-04 14:03:35 +0100

    feat: change api to openweathermap

--- AwesomeTSProject/App.tsx
+++ AwesomeTSProject/App.tsx
@@ -8,24 +8,20 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const Section: React.FC<{
   title: string;
@@ -54,10 +50,39 @@ const Section: React.FC<{
     </View>
   );
 };
+// récupère les données brutes depuis le serveur
+const getWeatherDataAsync = async (
+  latitudeQuery: string,
+  longitudeQuery: string,
+) => {
+  const longitude = parseFloat(longitudeQuery);
+  const latitude = parseFloat(latitudeQuery);
+  try {
+    //api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+    const requestURL = 'http://api.openweathermap.org/data/2.5/weather?';
+    const apiKey = 'd4ee1d5eb29009f743e4f5f7065d6797';
+    console.log(
+      `${requestURL}lat=${latitudeQuery}&lon=${longitudeQuery}&appid=${apiKey}`,
+    );
+    const response = await fetch(
+      `${requestURL}lat=${latitudeQuery}&lon=${longitudeQuery}&appid=${apiKey}`,
+    );
+
+    const jsonResponse = await response.json();
+    console.log(response);
+    console.log(jsonResponse);
+    return jsonResponse;
+  } catch (error) {
+    console.log(error);
+  }
+  return;
+};
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
-
+  const [latitude, setLatitude] = useState('37.3229978');
+  const [longitude, setLongitude] = useState('-122.0321823');
+  let jsonData;
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
@@ -68,25 +93,36 @@ const App = () => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type the latitude here"
+          onChangeText={newLatitude => setLatitude(newLatitude)}
+          defaultValue={latitude}
+        />
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type the latitude here"
+          onChangeText={newLongitude => setLongitude(newLongitude)}
+          defaultValue={longitude}
+        />
+        <Button
+          title="Search"
+          onPress={() => {
+            jsonData = getWeatherDataAsync(latitude, longitude);
+          }}
+        />
+
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+          <Section title="Summary">
+            Edit <Text style={styles.highlight}>App.tsx</Text>{' '}
+            {jsonData?.current?.weather?.description} to change this screen and
+            then come back to see your edits.
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Section title="Location"></Section>
+          <Section title="Details"></Section>
         </View>
       </ScrollView>
     </SafeAreaView>
